d0be04e9f8e0192a5fe0cc7c626e5953960389fe
sony <sony@Amit>
2023-01-11 16:19:37 +0530

    commit 2

--- Automation/src/amitsWorkspace/toclosechildwindow.java
+++ Automation/src/amitsWorkspace/toclosechildwindow.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class toclosechildwindow {
 
 	public static void main(String[] args) throws InterruptedException {
-
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
